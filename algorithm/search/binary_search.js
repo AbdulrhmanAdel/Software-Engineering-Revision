ee9a34b0001b1a08040f6e4ea26597a63cb7ce21
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2025-09-11 01:19:59 +0300

    Implement binary search algorithm

---
+++ algorithm/search/binary_search.js
@@ -0,0 +1,21 @@
+console.log(binarySearch([1, 5, 8, 10, 15, 60, 75, 100, 500], 3));
+
+function binarySearch(arr, searchFor) {
+  return _binarySearch(arr, searchFor, 0, arr.length - 1);
+}
+
+function _binarySearch(arr, searchFor, left, right) {
+  if (left > right) {
+    return -1;
+  }
+  const mid = Math.floor((left + right) / 2);
+  if (arr[mid] === searchFor) {
+    return mid;
+  }
+
+  if (arr[mid] > searchFor) {
+    return _binarySearch(arr, searchFor, left, mid - 1);
+  } else {
+    return _binarySearch(arr, searchFor, mid + 1, right);
+  }
+}
